2ad74b848922759d64382c74ad59f548e59b01f9
Josh Attenberg <josh.attenberg@gmail.com>
2022-08-16 13:18:49 -0400

    update

--- build.sh
+++ build.sh
@@ -14,23 +14,8 @@ fi
 venv="venv"
 echo "making virtualenv: $venv"
 
-echo "linking res library"
-ln -sf ../../../res .
-
 virtualenv $venv
 
 $venv/bin/pip install --upgrade pip pytest
 $venv/bin/pip install pip-tools
 $venv/bin/pip install -r src/requirements.txt
-
-echo "running pytest"
-PYTHONPATH=${pwd}/res:$PYTHONPATH $venv/bin/pytest
-
-path=`pwd`
-
-echo "useful aliases"
-echo "alias pr_py=\"PYTHONPATH=$path/res:$PYTHONPATH $path/$venv/bin/python\""
-echo "alias pr_pytest=\"PYTHONPATH=$path/res:$PYTHONPATH $path/$venv/bin/pytest\""
-
-echo "then run"
-echo "opt_py -m optimus_printer.main -d"
